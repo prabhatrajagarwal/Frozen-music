fdfed0f76bdbbf103fd40783160b8f00f0dfb29a
prabhatrajagarwal <ashubadclan6@gmail.com>
2025-04-06 19:24:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ addEventListener('fetch', event => {
 });
 
 // Add your bot token here.
-const BOT_TOKEN = "bot token here";
+const BOT_TOKEN = "7500118954:AAEv8fzJ-U7rN5xQrSdJGVaI8LC0abCu7Us";
 
 //Pro people: don't change this else your bot will be fucked.
 const API_BASE_URL = "https://probable-berti-frozenbotspvt-17e82b7b.koyeb.app";
